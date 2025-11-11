0cdd2fa166a5aca373f5b729ac177c1c12337463
MirkoRinke <mirkorinke@htp.com>
2025-11-11 01:06:25 +0100

    feat: implement community-home page with tech blocks and guest prompt

---
+++ src/app/pages/community-home/community-home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { TechBlock } from '../../components/tech-block/tech-block';
+
+import { AuthService } from '../../services/auth-service';
+import { GuestTeaserPrompt } from '../../components/guest-teaser-prompt/guest-teaser-prompt';
+
+@Component({
+  selector: 'app-community-home',
+  imports: [TechBlock, GuestTeaserPrompt],
+  templateUrl: './community-home.html',
+  styleUrl: './community-home.scss',
+})
+export class CommunityHome {
+  constructor(public authService: AuthService) {}
+}
