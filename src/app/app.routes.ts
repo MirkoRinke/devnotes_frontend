cc1f281ad03e8ed4c5f14eb6485cfb0b5f619b79
MirkoRinke <mirkorinke@htp.com>
2025-11-11 01:07:46 +0100

    feat: define application routes for Home, CommunityHome, and Login pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { CommunityHome } from './pages/community-home/community-home';
+import { Login } from './pages/login/login';
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'community-home', component: CommunityHome },
+  { path: 'login', component: Login },
+];
