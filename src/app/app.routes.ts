25be436a57aa6ee5c13f93ca0425a2438cc8b6ff
MirkoRinke <mirkorinke@htp.com>
2025-11-13 01:51:34 +0100

    feat: add route for PostTypesSelection component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { CommunityHome } from './pages/community-home/community-home';
 import { Login } from './pages/login/login';
+import { PostTypesSelection } from './pages/post-types-selection/post-types-selection';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'community-home', component: CommunityHome },
   { path: 'login', component: Login },
+  { path: 'post-types-selection', component: PostTypesSelection },
 ];
