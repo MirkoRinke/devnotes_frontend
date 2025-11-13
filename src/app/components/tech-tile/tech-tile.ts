1899589b11bf622c6b4c2195b7bec341530ef196
MirkoRinke <mirkorinke@htp.com>
2025-11-13 01:52:16 +0100

    feat: update TechTile component to include routing and query parameters

--- src/app/components/tech-tile/tech-tile.ts
+++ src/app/components/tech-tile/tech-tile.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tech-tile',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './tech-tile.html',
   styleUrl: './tech-tile.scss',
 })
 export class TechTile {
   @Input() name!: string;
   @Input() counts!: number;
+  @Input() endPoint!: string;
+  @Input() entity!: string;
 }
