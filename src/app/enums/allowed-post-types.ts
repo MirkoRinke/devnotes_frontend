916d591ac71848aeb6571898bd4434854d924d9a
MirkoRinke <mirkorinke@htp.com>
2025-11-15 23:01:39 +0100

    feat: expand AllowedPostTypesEnums to include individual post types.

--- src/app/enums/allowed-post-types.ts
+++ src/app/enums/allowed-post-types.ts
@@ -1,3 +1,9 @@
 export enum AllowedPostTypesEnums {
   ALL = 'tutorial,snippet,feedback,showcase,question,resources',
+  TUTORIAL = 'tutorial',
+  SNIPPET = 'snippet',
+  FEEDBACK = 'feedback',
+  SHOWCASE = 'showcase',
+  QUESTION = 'question',
+  RESOURCES = 'resources',
 }
