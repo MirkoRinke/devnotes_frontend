1bb53679c2c1805de0da7a90b7cc6589dddd06e3
MirkoRinke <mirkorinke@htp.com>
2025-11-15 23:12:21 +0100

    feat: enhance PostsList component to include entity validation and improve query parameter handling by include pagination params.

--- src/app/pages/posts-list/posts-list.ts
+++ src/app/pages/posts-list/posts-list.ts
@@ -10,6 +10,10 @@ import type { ApiResponseArrayInterface } from '../../interfaces/api-response';
 import type { PostInterface } from '../../interfaces/post';
 import type { PaginationInfoInterface } from '../../interfaces/pagination-info';
 
+import { ApiEndpointEnums } from '../../enums/api-endpoint';
+import { AllowedPostTypesEnums } from '../../enums/allowed-post-types';
+import { PostListAllowedEntitiesEnums } from '../../enums/post-list-allowed-entities';
+
 @Component({
   selector: 'app-posts-list',
   imports: [DatePipe],
@@ -31,39 +35,60 @@ export class PostsList {
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
-      const tech = params['tech'];
+      const entityValue = params['entityValue'];
       const entity = params['entity'];
-      const postType = params['postType'];
+      const postType = params['postType'] ? params['postType'] : AllowedPostTypesEnums.ALL;
+      const page = params['page'] ? params['page'] : '1';
+      const perPage = params['per_page'] ? params['per_page'] : '5';
 
       console.log('PostsList params:', params);
 
-      if (!tech || !postType || !entity) {
+      if (
+        !entityValue ||
+        !Object.values(PostListAllowedEntitiesEnums).includes(entity) ||
+        !Object.values(AllowedPostTypesEnums).includes(postType)
+      ) {
         this.router.navigate(['/']);
         return;
       }
 
-      this.selectedTech = tech;
+      this.selectedTech = entityValue;
       this.selectedPostType = postType;
-      this.getPostsList(tech, postType, entity);
+      this.getPostsList(entityValue, postType, entity, page, perPage);
     });
   }
 
-  getPostsList(tech: string, postType: string, entity: string) {
+  getPostsList(
+    entityValue: string,
+    postType: string,
+    entity: string,
+    page: string,
+    perPage: string
+  ) {
     const options = {
       params: new HttpParams()
-        .set(`filter[${entity}.name]`, `eq:${tech}`)
+        .set(`filter[${entity}.name]`, `eq:${entityValue}`)
         .set('filter[post_type]', postType)
-        .set('select', 'id,title,category,likes_count,comments_count,created_at'),
+        .set('select', 'id,title,category,likes_count,comments_count,created_at')
+        .set('page', page.toString())
+        .set('per_page', perPage.toString()),
     };
 
-    const url = `/posts/` + '?' + options.params.toString();
+    const url = ApiEndpointEnums.POSTS + '?' + options.params.toString();
+
+    console.log('Fetching PostsList with URL:', url);
 
     this.apiService.get<ApiResponseArrayInterface<PostInterface>>(url).subscribe({
       next: (response) => {
         this.postsList = response.data.data;
         this.paginationInfo = response.data as PaginationInfoInterface<PostInterface>;
-        console.log('PostsList response:', response.data);
-        console.log('Pagination info:', response.data);
+        console.log('PostsList response:', this.postsList);
+        console.log('Pagination info:', this.paginationInfo);
+
+        if (this.postsList.length === 0) {
+          this.router.navigate(['/']);
+          console.warn('No posts found for the selected criteria.');
+        }
       },
       error: (error) => {
         console.error('Error fetching posts list:', error);
