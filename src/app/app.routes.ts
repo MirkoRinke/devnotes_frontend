a303a8ed7363602f3ace88b17a251135d414c349
MirkoRinke <mirkorinke@htp.com>
2025-11-17 01:21:11 +0100

    feat: add route for Post component in app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { CommunityHome } from './pages/community-home/community-home';
 import { Login } from './pages/login/login';
 import { PostTypesSelection } from './pages/post-types-selection/post-types-selection';
 import { PostsList } from './pages/posts-list/posts-list';
+import { Post } from './pages/post/post';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -12,4 +13,5 @@ export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'post-types-selection', component: PostTypesSelection },
   { path: 'posts-list', component: PostsList },
+  { path: 'post', component: Post },
 ];
