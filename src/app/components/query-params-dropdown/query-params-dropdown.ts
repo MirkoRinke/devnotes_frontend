9b4faeb30d78a1586212c7b0112db0b2d2c9541a
MirkoRinke <mirkorinke@htp.com>
2025-11-20 03:37:54 +0100

    feat: enhance QueryParamsDropdown to utilize UsedTechnologiesService for dynamic dropdown values

--- src/app/components/query-params-dropdown/query-params-dropdown.ts
+++ src/app/components/query-params-dropdown/query-params-dropdown.ts
@@ -1,19 +1,41 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { UsedTechnologiesService } from '../../services/used-technologies.service';
 
 @Component({
   selector: 'app-query-params-dropdown',
   imports: [],
   templateUrl: './query-params-dropdown.html',
   styleUrl: './query-params-dropdown.scss',
+  providers: [UsedTechnologiesService],
 })
 export class QueryParamsDropdown {
-  @Input() dropdownValues!: string[];
-  @Input() key!: string;
   @Input() label!: string;
-  @Input() placeholder!: string;
+  @Input() key!: string;
+  @Input() defaultValue!: string | null;
 
-  constructor(private router: Router) {}
+  @Input() endPoint!: string;
+  @Input() params!: Array<string>;
+  @Input() values!: string[];
+
+  dropdownValues: string[] = [];
+
+  constructor(private router: Router, private usedTechnologiesService: UsedTechnologiesService) {}
+
+  ngOnInit() {
+    if (this.endPoint && this.params) {
+      this.getUsedTechnologies();
+    } else if (this.values) {
+      this.dropdownValues = this.values;
+    }
+  }
+
+  getUsedTechnologies() {
+    this.usedTechnologiesService.getUsedTechnologies(this.params, this.endPoint);
+    this.usedTechnologiesService.usedTechnologies$.subscribe((technologies) => {
+      this.dropdownValues = technologies.map((tech) => tech.name);
+    });
+  }
 
   onSelect(value: string) {
     if (value) {
