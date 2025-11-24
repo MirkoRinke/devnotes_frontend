dc825c61b8e9790b861676cddf8571c6e2771407
MirkoRinke <mirkorinke@htp.com>
2025-11-24 18:36:01 +0100

    refactor: enhance query parameter handling and validation in Post component

--- src/app/pages/post/post.ts
+++ src/app/pages/post/post.ts
@@ -7,10 +7,10 @@ import { ApiService } from '../../services/api.service';
 
 import type { ApiResponseObjektInterface } from '../../interfaces/api-response';
 import type { PostInterface } from '../../interfaces/post';
+import type { PostParamsInterface } from '../../interfaces/post-params';
+import type { Params } from '@angular/router';
 
 import { ApiEndpointEnums } from '../../enums/api-endpoint';
-import { AllowedPostTypesEnums } from '../../enums/allowed-post-types';
-import { RegexEnums } from '../../enums/regex';
 
 @Component({
   selector: 'app-post',
@@ -27,33 +27,90 @@ export class Post {
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
-      const postId = parseInt(params['post_id']);
-      const selectedEntityValue = params['selectedEntityValue'];
-      const selectedPostType = params['selectedPostType'];
+      const parsed: PostParamsInterface = this.parseQueryParams(params);
 
-      if (!Number.isInteger(postId) || !new RegExp(RegexEnums.entityValue).test(selectedEntityValue) || !Object.values(AllowedPostTypesEnums).includes(selectedPostType)) {
+      if (parsed.postId === null || !Number.isInteger(parsed.postId)) {
         this.router.navigate(['/']);
         console.warn('Missing required query parameters');
         return;
       }
 
-      this.getPost(postId);
-      this.selectedEntityValue = selectedEntityValue;
-      this.selectedPostType = selectedPostType;
+      this.setSelectedValues(parsed);
+      this.getPost(parsed.postId);
     });
   }
 
-  getPost(post_id: number): void {
+  /**
+   * Parse query params
+   *
+   * @param params
+   * @returns
+   */
+  private parseQueryParams(params: Params): PostParamsInterface {
+    return {
+      postId: parseInt(params['post_id']) ?? null,
+      selectedEntityValue: params['selectedEntityValue'] ?? null,
+      selectedPostType: params['selectedPostType'] ?? null,
+    };
+  }
+
+  /**
+   * Set selected values
+   *
+   * @param selectedEntityValue
+   * @param selectedPostType
+   */
+  private setSelectedValues(parsed: PostParamsInterface): void {
+    this.selectedEntityValue = parsed.selectedEntityValue;
+    this.selectedPostType = parsed.selectedPostType;
+  }
+
+  /**
+   * Get post by id from API
+   *
+   * @param postId
+   */
+  private getPost(post_id: number): void {
     const url = `${ApiEndpointEnums.POSTS}${post_id}`;
 
     this.apiService.get<ApiResponseObjektInterface<PostInterface>>(url).subscribe({
       next: (response) => {
         this.post = response.data.data;
+
+        if (this.checkAllowedEntityValue() || this.checkAllowedPostTypes()) {
+          this.router.navigate(['/']);
+          console.warn('Invalid entity value or post type');
+          return;
+        }
       },
       error: (error) => {
         this.router.navigate(['/']);
         console.error('Error fetching posts list:', error);
       },
     });
+  }
+
+  /**
+   * Check if selected entity value is allowed
+   *
+   * @returns boolean
+   */
+  private checkAllowedEntityValue(): boolean {
+    const postLanguages = this.post.languages?.map((language) => language.name) || [];
+    const postTechnologies = this.post.technologies?.map((technology) => technology.name) || [];
+    const postTags = this.post.tags?.map((tag) => tag.name) || [];
+
+    const allowedEntityValues: string[] = postLanguages.concat(postTechnologies, postTags);
+
+    return !allowedEntityValues.includes(this.selectedEntityValue!);
+  }
+
+  /**
+   * Check if selected post type is allowed
+   *
+   * @returns boolean
+   */
+  private checkAllowedPostTypes(): boolean {
+    return this.post.post_type !== this.selectedPostType;
   }
 }
