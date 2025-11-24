a73a478c5b4cd8cbe85000c8240186a56b95b4d8
MirkoRinke <mirkorinke@htp.com>
2025-11-24 18:36:20 +0100

    refactor: remove AllowedPostTypesEnums

--- src/app/enums/allowed-post-types.ts
+++
@@ -1,9 +0,0 @@
-export enum AllowedPostTypesEnums {
-  ALL = 'tutorial,snippet,feedback,showcase,question,resources',
-  TUTORIAL = 'tutorial',
-  SNIPPET = 'snippet',
-  FEEDBACK = 'feedback',
-  SHOWCASE = 'showcase',
-  QUESTION = 'question',
-  RESOURCES = 'resources',
-}
