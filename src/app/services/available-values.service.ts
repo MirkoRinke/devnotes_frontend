b9e41e4bf06822e6c70fc69ea66e5e5767e9942b
MirkoRinke <mirkorinke@htp.com>
2025-11-28 20:06:23 +0100

    docs: add JSDoc comments for getAvailableValues method in AvailableValuesService

--- src/app/services/available-values.service.ts
+++ src/app/services/available-values.service.ts
@@ -15,6 +15,13 @@ import { ApiService } from './api.service';
 export class AvailableValuesService {
   constructor(private apiService: ApiService) {}
 
+  /**
+   * Get available values from multiple endpoints as a single observable
+   *
+   * @param params e.g., ['?select=count:languages.name']" or ['?select=count:languages.name', '?select=count:technologies.name']
+   * @param endPoint e.g., /posts/ or /user-profiles/
+   * @returns Observable of AvailableValuesInterface array
+   */
   getAvailableValues(params: Array<string>, endPoint: string): Observable<AvailableValuesInterface[]> {
     const requests = params.map((param) => this.apiService.get<ApiResponseArrayInterface<AvailableValuesInterface>>(`${ApiEndpointEnums[endPoint as keyof typeof ApiEndpointEnums]}${param}`));
     return forkJoin(requests).pipe(map((responses) => responses.flatMap((response) => response.data.data)));
