6e1b71c6c669ad2073373ac3dea0e2b8b2f8dfff
MirkoRinke <mirkorinke@htp.com>
2025-11-30 19:42:05 +0100

    feat: add PostView component with template, styles, and tests

---
+++ src/app/components/post/post-view/post-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-view',
+  imports: [],
+  templateUrl: './post-view.html',
+  styleUrl: './post-view.scss',
+})
+export class PostView {
+
+}
