9d13e26ee1b6d3c2c61685abde30984d69489de4
MirkoRinke <mirkorinke@htp.com>
2025-11-30 21:42:00 +0100

    feat: enhance PostView component with additional post details and user avatar handling (Temporary)

--- src/app/components/post/post-view/post-view.ts
+++ src/app/components/post/post-view/post-view.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import type { PostInterface } from '../../../interfaces/post';
 
 @Component({
   selector: 'app-post-view',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './post-view.html',
   styleUrl: './post-view.scss',
 })
 export class PostView {
+  @Input() post!: PostInterface;
 
+  avatarItemsEntries() {
+    return this.post?.user?.avatar_items ? Object.entries(this.post.user.avatar_items).map(([key, url]) => ({ key, url })) : [];
+  }
 }
