acc6c2505dd271535e2f1177abc2372d919b009d
MirkoRinke <mirkorinke@htp.com>
2025-11-30 22:18:57 +0100

    feat: add PageNavigation component with template, styles, and tests

---
+++ src/app/components/page-navigation/page-navigation.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-page-navigation',
+  imports: [RouterModule],
+  templateUrl: './page-navigation.html',
+  styleUrl: './page-navigation.scss',
+})
+export class PageNavigation {}
