193a96803044a6e17e1e320d14cddecfa3eb328b
MirkoRinke <mirkorinke@htp.com>
2025-11-30 22:19:09 +0100

    feat: replace temporary navigation buttons with PageNavigation component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterModule } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { PageNavigation } from './components/page-navigation/page-navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, PageNavigation],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
