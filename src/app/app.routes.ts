7714b5b1fd301f0a5e9b64754e342af1a2c6d305
MirkoRinke <mirkorinke@htp.com>
2025-12-03 20:54:21 +0100

    feat: add routes for MyArea, Favorites, and Network components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,15 @@ import { Login } from './pages/login/login';
 import { PostTypesSelection } from './pages/post-types-selection/post-types-selection';
 import { PostsList } from './pages/posts-list/posts-list';
 import { Post } from './pages/post/post';
+import { Favorites } from './pages/favorites/favorites';
+import { Network } from './pages/network/network';
+import { MyArea } from './pages/my-area/my-area';
 
 export const routes: Routes = [
   { path: '', component: Home },
+  { path: 'my-area', component: MyArea },
+  { path: 'favorites', component: Favorites },
+  { path: 'network', component: Network },
   { path: 'community', component: Community },
   { path: 'login', component: Login },
   { path: 'post-types-selection', component: PostTypesSelection },
