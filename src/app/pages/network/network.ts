94c029d1e474e9f5310782edb87cd8f0e0b6f745
MirkoRinke <mirkorinke@htp.com>
2025-12-03 20:53:45 +0100

    feat: add initial implementation of network component with template and styles

---
+++ src/app/pages/network/network.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-network',
+  imports: [],
+  templateUrl: './network.html',
+  styleUrl: './network.scss',
+})
+export class Network {
+
+}
