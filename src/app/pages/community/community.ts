5ac10aaaa85019eeca0a4037649b0957402723a3
MirkoRinke <mirkorinke@htp.com>
2025-12-14 04:45:13 +0100

    feat: refactor Community component to manage search results with proper cleanup on destroy

--- src/app/pages/community/community.ts
+++ src/app/pages/community/community.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { TechBlock } from '../../components/tech-block/tech-block';
 import { GuestTeaserPrompt } from '../../components/guest-teaser-prompt/guest-teaser-prompt';
@@ -16,17 +18,24 @@ export class Community {
   searchActive: boolean = false;
   searchResultsLoaded: boolean = false;
 
+  private destroy$ = new Subject<void>();
+
   constructor(
     public authService: AuthService,
     private searchService: SearchService,
   ) {}
 
   ngOnInit() {
-    this.ngOnChanges();
+    this.searchResult();
   }
 
-  ngOnChanges() {
-    this.searchService.searchValue.subscribe((inputValue) => {
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  searchResult() {
+    this.searchService.searchValue.pipe(takeUntil(this.destroy$)).subscribe((inputValue) => {
       this.searchActive = inputValue.length > 0;
       if (this.searchActive && !this.searchResultsLoaded) {
         this.searchResultsLoaded = true;
