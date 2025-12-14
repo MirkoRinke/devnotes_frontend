2b6f8c813604ad3c9391026c1301825cd0dcc7df
MirkoRinke <mirkorinke@htp.com>
2025-12-14 07:01:28 +0100

    feat: extend SearchService with methods to manage loading state and data loaded status

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -7,14 +7,44 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class SearchService {
   searchValue = new BehaviorSubject<string>('');
+  loadSearchResults = new BehaviorSubject<boolean>(false);
+  dataLoaded = new BehaviorSubject<boolean>(false);
 
-  constructor() {}
-
+  /**
+   * Set search value
+   *
+   * @param value
+   */
   searchValueInput(value: string) {
     this.searchValue.next(value);
   }
 
+  /**
+   * Clear search data and enable loading of search results
+   */
   clear() {
     this.searchValue.next('');
+    this.loadSearchResults.next(false);
+  }
+
+  /**
+   * Enable loading of search results
+   */
+  enableLoad() {
+    this.loadSearchResults.next(true);
+  }
+
+  /**
+   * Clear data loaded state
+   */
+  clearDataLoaded() {
+    this.dataLoaded.next(false);
+  }
+
+  /**
+   * Mark data as loaded
+   */
+  dataLoadedComplete() {
+    this.dataLoaded.next(true);
   }
 }
