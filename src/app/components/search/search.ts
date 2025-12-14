4d2297d347ff2eda541c27acb3f38e725524c066
MirkoRinke <mirkorinke@htp.com>
2025-12-14 04:39:08 +0100

    feat: implement router event handling in Search component to clear search input on navigation

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
+
 import { SvgIconsService } from '../../services/svg.icons.service';
 import { SearchService } from '../../services/search.service';
 
@@ -11,13 +15,36 @@ import { SearchService } from '../../services/search.service';
 export class Search {
   inputFeldVisible: boolean = false;
 
+  private destroy$ = new Subject<void>();
+
   constructor(
     public svgIconsService: SvgIconsService,
     public searchService: SearchService,
+    private router: Router,
   ) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationStart),
+        takeUntil(this.destroy$),
+      )
+      .subscribe(() => {
+        this.inputFeldVisible = false;
+        this.searchService.clear();
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   toggleInputFeld() {
     this.inputFeldVisible = !this.inputFeldVisible;
+    if (!this.inputFeldVisible) {
+      this.searchService.clear();
+    }
   }
 
   handelSearch(inputValue: string) {
