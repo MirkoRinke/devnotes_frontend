88e7436e30ebd17565e960453d8ef2f360c084dd
MirkoRinke <mirkorinke@htp.com>
2025-12-14 04:04:14 +0100

    feat: implement SearchService with BehaviorSubject for search value management

---
+++ src/app/services/search.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SearchService {
+  searchValue = new BehaviorSubject<string>('');
+
+  constructor() {}
+
+  searchValueInput(value: string) {
+    this.searchValue.next(value);
+  }
+}
