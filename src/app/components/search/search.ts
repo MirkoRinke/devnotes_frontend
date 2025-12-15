d1f966dcba113327337e3d9909c11cafb2f37294
MirkoRinke <mirkorinke@htp.com>
2025-12-15 04:10:16 +0100

    feat: simplify search component by removing unused code and enhancing input handling

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,73 +1,18 @@
 import { Component } from '@angular/core';
-import { Router, NavigationStart } from '@angular/router';
-import { Subject } from 'rxjs';
-import { filter, takeUntil } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 import { SvgIconsService } from '../../services/svg.icons.service';
 import { SearchService } from '../../services/search.service';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search.html',
   styleUrl: './search.scss',
 })
 export class Search {
-  inputFeldVisible: boolean = false;
-  dataLoaded: boolean = false;
-
-  private destroy$ = new Subject<void>();
-
   constructor(
     public svgIconsService: SvgIconsService,
     public searchService: SearchService,
-    private router: Router,
   ) {}
-
-  ngOnInit() {
-    this.router.events
-      .pipe(
-        filter((event) => event instanceof NavigationStart),
-        takeUntil(this.destroy$),
-      )
-      .subscribe(() => {
-        this.inputFeldVisible = false;
-        this.searchService.clear();
-      });
-    this.dataLoadedComplete();
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  /**
-   * Toggle the visibility of the search input field
-   */
-  toggleInputFeld() {
-    this.inputFeldVisible = !this.inputFeldVisible;
-    this.searchService.enableLoad();
-    if (!this.inputFeldVisible) {
-      this.searchService.clear();
-    }
-  }
-
-  /**
-   * Handle search input changes
-   *
-   * @param inputValue
-   */
-  handelSearch(inputValue: string) {
-    this.searchService.searchValueInput(inputValue);
-  }
-
-  /**
-   * Subscribe to dataLoaded observable to update local dataLoaded state
-   */
-  dataLoadedComplete() {
-    this.searchService.dataLoaded.pipe(takeUntil(this.destroy$)).subscribe((loaded) => {
-      this.dataLoaded = loaded;
-    });
-  }
 }
