6d1016950ac55603e77359398abd43266199c6aa
MirkoRinke <mirkorinke@htp.com>
2025-12-18 01:16:48 +0100

    feat: add search input reference and implement cleanup on component destroy

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,5 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, NavigationStart } from '@angular/router';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { SvgIconsService } from '../../services/svg.icons.service';
 import { SearchService } from '../../services/search.service';
@@ -10,9 +14,30 @@ import { SearchService } from '../../services/search.service';
   templateUrl: './search.html',
   styleUrl: './search.scss',
 })
-export class Search {
+export class Search implements OnDestroy {
+  @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>;
+
+  private destroy$ = new Subject<void>();
+
   constructor(
     public svgIconsService: SvgIconsService,
     public searchService: SearchService,
+    private router: Router,
   ) {}
+
+  ngAfterViewInit(): void {
+    this.router.events.pipe(takeUntil(this.destroy$)).subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        if (this.searchInput && this.searchInput.nativeElement) {
+          this.searchInput.nativeElement.value = '';
+          this.searchService.searchValueInput('');
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
