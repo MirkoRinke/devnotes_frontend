720bfa38e00aa83f503cb0728140e9171e40dee4
MirkoRinke <mirkorinke@htp.com>
2026-02-04 15:52:33 +0100

    feat: refactor user badge menu to use separate component for improved structure and maintainability

--- src/app/components/user-badge/user-badge.ts
+++ src/app/components/user-badge/user-badge.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 
 import type { UserInterface } from '../../interfaces/user';
+import { UserBadgeMenu } from './user-badge-menu/user-badge-menu';
 
 @Component({
   selector: 'app-user-badge',
-  imports: [],
+  imports: [UserBadgeMenu],
   templateUrl: './user-badge.html',
   styleUrl: './user-badge.scss',
 })
@@ -31,35 +32,10 @@ export class UserBadge {
   }
 
   /**
-   * Handle animation end events
-   *
-   * @param event
+   * Close Badge Menu from menu component
    */
-  onAnimationEnd(event: AnimationEvent) {
-    //TODO Refactoring this later to 'fade-out'
-    if (event.animationName.endsWith('animated-out') && this.isUserBadgeMenuOpen) {
-      this.isUserBadgeMenuOpen = false;
-    }
-  }
-
-  /**
-   * User Profile Clicked (Placeholder)
-   */
-  userProfile() {
-    alert('User Profile clicked');
-  }
-
-  /**
-   * Toggle Follow User (Placeholder)
-   */
-  toggleFollowUser() {
-    alert('Toggle Follow User clicked');
-  }
-
-  /**
-   * Report User (Placeholder)
-   */
-  reportUser() {
-    alert('Report User clicked');
+  closeBadgeMenu() {
+    this.isUserBadgeMenuOpen = false;
+    this.isUserBadgeMenuAnimating = false;
   }
 }
