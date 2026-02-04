dd6a8394d7e1fe78444d1b321280a6ce5f7e7c93
MirkoRinke <mirkorinke@htp.com>
2026-02-04 17:23:46 +0100

    feat: replace version dropdown implementation with a dedicated component for improved maintainability

--- src/app/components/post/post-view/post-view.ts
+++ src/app/components/post/post-view/post-view.ts
@@ -3,20 +3,18 @@ import { Component, Input } from '@angular/core';
 import type { PostInterface } from '../../../interfaces/post';
 import type { PostResourceModalInterface } from '../../../interfaces/post-ressource-modal';
 
-import { LocalDatePipe } from '../../../pipes/local-date-pipe';
-
 import { PostResourceModal } from './post-resource-modal/post-resource-modal';
 
-import { DateFormatterService } from '../../../services/date.formatter.service';
 import { SvgIconsService } from '../../../services/svg.icons.service';
 import { AuthService } from '../../../services/auth.service';
 import { PostSettingsDropdown } from './post-settings-dropdown/post-settings-dropdown';
 import { ReportModal } from '../../report-modal/report-modal';
 import { UserBadge } from '../../user-badge/user-badge';
+import { PostVersionDropdown } from './post-version-dropdown/post-version-dropdown';
 
 @Component({
   selector: 'app-post-view',
-  imports: [LocalDatePipe, PostResourceModal, PostSettingsDropdown, ReportModal, UserBadge],
+  imports: [PostResourceModal, PostSettingsDropdown, ReportModal, UserBadge, PostVersionDropdown],
   templateUrl: './post-view.html',
   styleUrl: './post-view.scss',
 })
@@ -27,31 +25,23 @@ export class PostView {
 
   currentPost!: PostInterface;
 
-  postVersionsValues: Array<{ label: string; value: number }> = [];
-
-  showDropdownValues = false;
-  showDropdownAnimation = false;
-
+  currentPostModal: PostResourceModalInterface = { title: '', resources: [], previews: [] };
   isPostResourceModalOpen = false;
   isPostResourceModalAnimating = false;
 
   isReportModalOpen = false;
   isReportModalAnimating = false;
 
-  currentPostModal: PostResourceModalInterface = { title: '', resources: [], previews: [] };
-
   isCopied = false;
   copiedFailed = false;
 
   constructor(
     public svgIconsService: SvgIconsService,
-    private dateFormatterService: DateFormatterService,
     public authService: AuthService,
   ) {}
 
   ngOnInit() {
     this.currentPost = this.post;
-    this.createPostVersionsValues();
   }
 
   /**
@@ -61,35 +51,6 @@ export class PostView {
    */
   avatarItemsEntries() {
     return this.post?.user?.avatar_items ? Object.entries(this.post.user.avatar_items).map(([key, url]) => ({ key, url })) : [];
-  }
-
-  /**
-   * Create post versions values for selector
-   */
-  createPostVersionsValues() {
-    this.postVersionsValues = [{ label: this.dateFormatterService.toLocaleString(this.post.updated_at), value: -1 }];
-    if (this.post.history && this.post.history.length > 0) {
-      this.post.history.forEach((version, index) => {
-        const label = `${this.dateFormatterService.toLocaleString(version.updated_at)}`;
-        this.postVersionsValues.push({ label, value: index });
-      });
-    }
-  }
-
-  /**
-   * Toggle post version dropdown
-   */
-  togglePostVersionDropdown() {
-    if (this.postVersionsValues.length <= 1) {
-      return;
-    }
-
-    if (this.showDropdownValues) {
-      this.showDropdownAnimation = false;
-    } else {
-      this.showDropdownValues = true;
-      requestAnimationFrame(() => (this.showDropdownAnimation = true));
-    }
   }
 
   /**
@@ -135,11 +96,17 @@ export class PostView {
     this.isPostResourceModalAnimating = false;
   }
 
+  /**
+   * Open Report Modal
+   */
   openReportModal() {
     this.isReportModalOpen = true;
     requestAnimationFrame(() => (this.isReportModalAnimating = true));
   }
 
+  /**
+   * Close Report Modal
+   */
   closeReportModal() {
     this.isReportModalAnimating = false;
   }
@@ -150,11 +117,6 @@ export class PostView {
    * @param event
    */
   onAnimationEnd(event: AnimationEvent) {
-    //TODO Refactoring this later to 'fade-out'
-    if (event.animationName.endsWith('animated-out') && this.showDropdownValues) {
-      this.showDropdownValues = false;
-    }
-
     if (event.animationName.endsWith('fade-out')) {
       if (this.isPostResourceModalOpen) {
         this.isPostResourceModalOpen = false;
@@ -174,9 +136,6 @@ export class PostView {
    */
   onSelect(versionIndex: number) {
     if (this.post.history && versionIndex >= 0 && this.post.history.length > versionIndex) {
-      console.log(this.post.history[versionIndex]);
-      console.log(this.currentPost);
-
       this.currentPost = {
         ...this.post,
         ...this.post.history[versionIndex],
