b276ef6e9ab227025375cb6c96e72f82387876d3
MirkoRinke <mirkorinke@htp.com>
2026-02-04 15:51:29 +0100

    feat: implement user badge menu with profile, follow, and report

--- src/app/components/user-badge/user-badge-menu/user-badge-menu.ts
+++ src/app/components/user-badge/user-badge-menu/user-badge-menu.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import type { UserInterface } from '../../../interfaces/user';
 
 @Component({
   selector: 'app-user-badge-menu',
@@ -6,4 +8,51 @@ import { Component } from '@angular/core';
   templateUrl: './user-badge-menu.html',
   styleUrl: './user-badge-menu.scss',
 })
-export class UserBadgeMenu {}
+export class UserBadgeMenu {
+  @Input() user!: UserInterface | null;
+
+  @Input() isUserBadgeMenuOpen: boolean = false;
+  @Input() isUserBadgeMenuAnimating: boolean = false;
+
+  @Output() closeMenu = new EventEmitter<void>();
+
+  /**
+   * Handle close event from parent component
+   * triggered onAnimationEnd when animated out
+   */
+  onClose() {
+    this.isUserBadgeMenuAnimating = false;
+  }
+
+  /**
+   * Handle animation end events
+   *
+   * @param event
+   */
+  onAnimationEnd(event: AnimationEvent) {
+    if (event.animationName.endsWith('animated-out')) {
+      this.closeMenu.emit();
+    }
+  }
+
+  /**
+   * TODO: Implement navigation to user profile
+   */
+  userProfile() {
+    console.log('User Profile clicked');
+  }
+
+  /**
+   * TODO: Implement follow/unfollow user functionality
+   */
+  toggleFollowUser() {
+    console.log('Toggle Follow User clicked');
+  }
+
+  /**
+   * TODO: Implement report user functionality
+   */
+  reportUser() {
+    console.log('Report User clicked');
+  }
+}
