660081903cbf867f854c01d890e46dcfa4d04991
MirkoRinke <mirkorinke@htp.com>
2026-02-13 19:23:57 +0100

    feat: add TOGGLE_FAVORITE_SUFFIX endpoint to ApiEndpointEnums

--- src/app/enums/api-endpoint.ts
+++ src/app/enums/api-endpoint.ts
@@ -2,4 +2,5 @@ export enum ApiEndpointEnums {
   POSTS = '/posts/',
   FAVORITE_TECH_STACK = '/user-profiles/',
   FAVORITE_POSTS = '/user/favorites/posts/',
+  TOGGLE_FAVORITE_SUFFIX = '/favorites/',
 }
