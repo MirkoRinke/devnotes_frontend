2fe1b32aaeb9b956cfda87043ab1ba0b7f6ecff6
MirkoRinke <mirkorinke@htp.com>
2026-02-14 21:14:18 +0100

    feat: integrate Prism.js for syntax highlighting and update asset paths

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+
+import * as Prism from 'prismjs';
+import 'prismjs/plugins/autoloader/prism-autoloader';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,13 @@ import { Footer } from './components/footer/footer';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('devnotes_frontend');
+
+  ngOnInit() {
+    const prism = Prism as any;
+    if (prism.plugins && prism.plugins.autoloader) {
+      prism.plugins.autoloader.languages_path = 'assets/prism/components/';
+    }
+  }
 }
