9ddc92b3f5b8e29a0079fd9ab12b7dfb6a7883f0
MirkoRinke <mirkorinke@htp.com>
2026-02-14 21:21:14 +0100

    feat: enhance code highlighting with Prism.js integration and dynamic updates

--- src/app/components/post/post-view/post-code/post-code.ts
+++ src/app/components/post/post-view/post-code/post-code.ts
@@ -1,20 +1,54 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
+import * as Prism from 'prismjs';
 
 import { SvgIconsService } from '../../../../services/svg.icons.service';
 
+import { PrismLanguagePipe } from '../../../../pipes/prism-language-pipe';
+
 @Component({
   selector: 'app-post-code',
-  imports: [],
+  imports: [PrismLanguagePipe],
   templateUrl: './post-code.html',
   styleUrl: './post-code.scss',
 })
-export class PostCode {
+export class PostCode implements OnChanges, AfterViewInit {
+  @ViewChild('codeElement') codeElement: ElementRef | undefined;
+  @Input() language: string | null = null;
   @Input() code: string | null = null;
 
   isCopied = false;
   copiedFailed = false;
 
   constructor(public svgIconsService: SvgIconsService) {}
+
+  /**
+   * Highlight code after view initialization
+   */
+  ngAfterViewInit() {
+    this.highlight();
+  }
+
+  /**
+   * Detect changes in code or language and re-highlight if necessary
+   *
+   * @param changes
+   */
+  ngOnChanges(changes: SimpleChanges) {
+    if ((changes['code'] && !changes['code'].firstChange) || (changes['language'] && !changes['language'].firstChange)) {
+      this.highlight();
+    }
+  }
+
+  /**
+   * Highlight code using Prism.js
+   */
+  private highlight() {
+    if (this.codeElement?.nativeElement) {
+      this.codeElement.nativeElement.innerHTML = this.code || '';
+      Prism.highlightElement(this.codeElement.nativeElement);
+    }
+  }
 
   /**
    * Copy code to clipboard
