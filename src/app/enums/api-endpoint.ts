26f4d94ffef3b15d78e07476dca0ca0a10852ee5
MirkoRinke <mirkorinke@htp.com>
2026-02-16 22:34:04 +0100

    feat: add USER_PROFILES endpoint to ApiEndpointEnums

--- src/app/enums/api-endpoint.ts
+++ src/app/enums/api-endpoint.ts
@@ -3,4 +3,5 @@ export enum ApiEndpointEnums {
   FAVORITE_TECH_STACK = '/user-profiles/',
   FAVORITE_POSTS = '/user/favorites/posts/',
   TOGGLE_FAVORITE_SUFFIX = '/favorites/',
+  USER_PROFILES = '/user-profiles/',
 }
