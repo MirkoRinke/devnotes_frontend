80cc075c7129b0b3b8b1ecfe407d5bef7ec390d1
MirkoRinke <mirkorinke@htp.com>
2026-02-21 18:37:42 +0100

    feat: add missing LIKES and REPORT endpoints to ApiEndpointEnums

--- src/app/enums/api-endpoint.ts
+++ src/app/enums/api-endpoint.ts
@@ -2,6 +2,8 @@ export enum ApiEndpointEnums {
   POSTS = '/posts/',
   FAVORITE_TECH_STACK = '/user-profiles/',
   FAVORITE_POSTS = '/user/favorites/posts/',
-  TOGGLE_FAVORITE_SUFFIX = '/favorites/',
   USER_PROFILES = '/user-profiles/',
+  TOGGLE_FAVORITE_SUFFIX = '/favorites/',
+  LIKES = '/likes',
+  REPORT = '/reports',
 }
