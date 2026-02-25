2727867edde13badfecb49259395cc479a30c953
MirkoRinke <mirkorinke@htp.com>
2026-02-25 18:55:18 +0100

    feat: add selectedEntity property to PostParamsInterface for enhanced data handling

--- src/app/interfaces/post-params.ts
+++ src/app/interfaces/post-params.ts
@@ -1,6 +1,7 @@
 export interface PostParamsInterface {
   postId: number | null;
   selectedEntityValue: string | null;
+  selectedEntity: string | null;
   selectedPostType: string | null;
   context: string | null;
 }
