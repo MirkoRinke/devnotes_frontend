1635ea23ca01b52bb3b8f2382768ec1ceaff9ab6
MirkoRinke <mirkorinke@htp.com>
2026-03-03 20:53:32 +0100

    feat(translations): add German translations for post types

---
+++ src/app/i18n/data/translations.de.ts
@@ -0,0 +1,34 @@
+import { Content } from './translation.interface';
+
+export const CONTENT_DE: Content = {
+  PostTypes: {
+    all_types: {
+      title: 'Alle Typen',
+      description: 'Alle verfügbaren Beitragstypen auf einen Blick.',
+    },
+    feedback: {
+      title: 'Feedback',
+      description: 'Feedback geben & erhalten. Teile deine Projekte oder biete wertvolle Rückmeldungen zur Verbesserung.',
+    },
+    questions: {
+      title: 'Fragen',
+      description: 'Fragen & Antworten. Stelle gezielte Fragen und erhalte hilfreiche Antworten von der Community.',
+    },
+    resources: {
+      title: 'Ressourcen',
+      description: 'Wissensbasis & Ressourcen. Teile hilfreiche Materialien, um das Wissen der Community zu erweitern.',
+    },
+    showcase: {
+      title: 'Showcase',
+      description: 'Projekte & Erfolge. Präsentiere deine fertigen Projekte und teile deine Erfolge mit anderen.',
+    },
+    snippets: {
+      title: 'Snippets',
+      description: 'Code Snippets. Teile nützliche Code-Schnipsel oder finde Lösungen für deine aktuellen Probleme.',
+    },
+    tutorials: {
+      title: 'Tutorials',
+      description: 'Anleitungen & Tutorials. Finde oder erstelle leicht verständliche Anleitungen für neue Skills.',
+    },
+  },
+};
