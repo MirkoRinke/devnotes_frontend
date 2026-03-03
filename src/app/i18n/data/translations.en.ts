04f2a5f7805bd0a6d14851d093ec36fe6fe3a520
MirkoRinke <mirkorinke@htp.com>
2026-03-03 20:53:40 +0100

    feat(translations): add English translations for post types

---
+++ src/app/i18n/data/translations.en.ts
@@ -0,0 +1,34 @@
+import { Content } from './translation.interface';
+
+export const CONTENT_EN: Content = {
+  PostTypes: {
+    all_types: {
+      title: 'All Types',
+      description: 'All available post types at a glance.',
+    },
+    feedback: {
+      title: 'Feedback',
+      description: 'Give & receive feedback. Share your projects or provide valuable feedback for improvement.',
+    },
+    questions: {
+      title: 'Questions',
+      description: 'Questions & Answers. Ask targeted questions and receive helpful answers from the community.',
+    },
+    resources: {
+      title: 'Resources',
+      description: "Knowledge base & resources. Share helpful materials to expand the community's knowledge.",
+    },
+    showcase: {
+      title: 'Showcase',
+      description: 'Projects & Achievements. Showcase your completed projects and share your achievements with others.',
+    },
+    snippets: {
+      title: 'Snippets',
+      description: 'Code Snippets. Share useful code snippets or find solutions for your current problems.',
+    },
+    tutorials: {
+      title: 'Tutorials',
+      description: 'Guides & Tutorials. Find or create easy-to-understand guides for new skills.',
+    },
+  },
+};
