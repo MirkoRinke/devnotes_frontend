f560b44a88662d52e25349cb94ec54dedd1d962a
MirkoRinke <mirkorinke@htp.com>
2026-03-09 18:23:07 +0100

    feat(post-resource-modal): implement external content enabling functionality and update modal structure

--- src/app/components/post/post-view/post-resource-modal/post-resource-modal.ts
+++ src/app/components/post/post-view/post-resource-modal/post-resource-modal.ts
@@ -1,9 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
-import type { PostResourceModalInterface } from '../../../../interfaces/post-ressource-modal';
+import type { PostResourceModalInterface, PostResourceType } from '../../../../interfaces/post-ressource-modal';
 
+import { ApiService } from '../../../../services/api.service';
+import { AuthService } from '../../../../services/auth.service';
 import { SvgIconsService } from '../../../../services/svg.icons.service';
 
+import { ApiEndpointEnums } from '../../../../enums/api-endpoint';
 @Component({
   selector: 'app-post-resource-modal',
   imports: [],
@@ -12,12 +15,47 @@ import { SvgIconsService } from '../../../../services/svg.icons.service';
 })
 export class PostResourceModal {
   @Input() modalData!: PostResourceModalInterface;
-
   @Output() closeModal = new EventEmitter<void>();
 
-  constructor(public svgIconsService: SvgIconsService) {}
+  @Output() externalContentEnabled = new EventEmitter<PostResourceType>();
+
+  isProcessing = false;
+
+  constructor(
+    public svgIconsService: SvgIconsService,
+    private apiService: ApiService,
+    private authService: AuthService,
+  ) {}
 
   onClose() {
     this.closeModal.emit();
+  }
+
+  enableExternalContent(type: PostResourceType, hours: number) {
+    /**
+     * Prevent multiple requests and ensure user is logged in and type is valid
+     */
+    if (this.isProcessing || !this.authService.isLoggedIn() || type === null) {
+      return;
+    }
+
+    this.isProcessing = true;
+
+    const url = `${ApiEndpointEnums.USER_PROFILES}${this.authService.getCurrentUserId()}${ApiEndpointEnums.ENABLE_TEMPORARY_EXTERNALS_SUFFIX}`;
+
+    let data = {
+      type: type,
+      hours: hours,
+    };
+
+    this.apiService.post(url, data).subscribe({
+      next: (response) => {
+        this.isProcessing = false;
+        this.externalContentEnabled.emit(type);
+      },
+      error: (error) => {
+        this.isProcessing = false;
+      },
+    });
   }
 }
