8b79c2826f8b8bad8ef376309865754ff8c835b1
MirkoRinke <mirkorinke@htp.com>
2026-03-09 18:19:49 +0100

    fix(post-resource-modal): add type field and PostResourceType definition

--- src/app/interfaces/post-ressource-modal.ts
+++ src/app/interfaces/post-ressource-modal.ts
@@ -2,6 +2,9 @@ import { ExternalSourcePreviewInterface } from './post';
 
 export interface PostResourceModalInterface {
   title: string;
+  type: PostResourceType;
   resources: string[];
   previews: ExternalSourcePreviewInterface[];
 }
+
+export type PostResourceType = 'images' | 'videos' | 'resources' | null;
