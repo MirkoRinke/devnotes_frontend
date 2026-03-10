19bb1038cdfb25e08eae9642a8965ef734379ef3
MirkoRinke <mirkorinke@htp.com>
2026-03-10 18:38:52 +0100

    feat(post-form): implement post form structure and styling

--- src/app/components/post/post-form/post-form.ts
+++ src/app/components/post/post-form/post-form.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+import type { PostInterface } from '../../../interfaces/post';
+
 @Component({
   selector: 'app-post-form',
   imports: [],
@@ -8,6 +10,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 })
 export class PostForm {
   @Input() mode: 'create' | 'edit' = 'create';
+  @Input() post: PostInterface | null = null;
 
   @Output() modeChange = new EventEmitter<'view'>();
 
