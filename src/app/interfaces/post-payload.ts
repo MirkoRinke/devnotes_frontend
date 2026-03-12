af8c616a9738b59b79724f48a35e23444e0c70e0
MirkoRinke <mirkorinke@htp.com>
2026-03-12 20:58:24 +0100

    feat(post-payload): add PostPayload interface for post data structure

---
+++ src/app/interfaces/post-payload.ts
@@ -0,0 +1,14 @@
+export interface PostPayload {
+  title: string;
+  code: string;
+  description: string;
+  images: string[];
+  videos: string[];
+  resources: string[];
+  languages: string[];
+  technologies: string[];
+  tags: string[];
+  category: string;
+  post_type: string;
+  status: string;
+}
