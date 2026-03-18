d52624d5c61d6d8123a801917ff45581456d46e1
MirkoRinke <mirkorinke@htp.com>
2026-03-18 01:23:46 +0100

    feat(post-code): enhance textarea functionality and improve view mode handling

--- src/app/components/post/post-view/post-code/post-code.ts
+++ src/app/components/post/post-view/post-code/post-code.ts
@@ -1,33 +1,39 @@
-import { Component, Input, OnChanges, SimpleChanges, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, Input, Output, OnChanges, SimpleChanges, ViewChild, ElementRef, EventEmitter } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 import * as Prism from 'prismjs';
+import { PrismLanguagePipe } from '../../../../pipes/prism-language-pipe';
 
 import { SvgIconsService } from '../../../../services/svg.icons.service';
-
-import { PrismLanguagePipe } from '../../../../pipes/prism-language-pipe';
+import { getCssVariableValue } from '../../../../utils/css-helper';
 
 @Component({
   selector: 'app-post-code',
-  imports: [PrismLanguagePipe],
+  imports: [PrismLanguagePipe, ReactiveFormsModule],
   templateUrl: './post-code.html',
   styleUrl: './post-code.scss',
 })
-export class PostCode implements OnChanges, AfterViewInit {
-  @ViewChild('codeElement') codeElement: ElementRef | undefined;
+export class PostCode implements OnChanges {
+  @Input() isViewMode = true;
+  @Input() control: FormControl | null = null;
+
   @Input() language: string | null = null;
   @Input() code: string | null = null;
+
+  isFirstLoad = true;
 
   isCopied = false;
   copiedFailed = false;
 
-  constructor(public svgIconsService: SvgIconsService) {}
+  codeElement: ElementRef | null = null;
+  codeTextarea: ElementRef | null = null;
 
-  /**
-   * Highlight code after view initialization
-   */
-  ngAfterViewInit() {
-    this.highlight();
-  }
+  maxHeight: number | null = null;
+  minHeight: number = 0;
+
+  @Output() viewModeChange = new EventEmitter<boolean>();
+
+  constructor(public svgIconsService: SvgIconsService) {}
 
   /**
    * Detect changes in code or language and re-highlight if necessary
@@ -64,5 +70,77 @@ export class PostCode implements OnChanges, AfterViewInit {
         this.copiedFailed = true;
         setTimeout(() => (this.copiedFailed = false), 2000);
       });
+  }
+
+  /**
+   * Sets the reference to the code element and highlights it.
+   *
+   * @param content The ElementRef of the code element.
+   *
+   */
+  @ViewChild('codeElement') set codeElementRef(content: ElementRef) {
+    this.codeElement = content;
+    if (content) {
+      requestAnimationFrame(() => {
+        if (this.control && this.isFirstLoad) {
+          this.viewModeChange.emit();
+          this.isFirstLoad = false;
+        }
+        this.highlight();
+      });
+
+      // console.log('codeElement:', this.codeElement?.nativeElement.getBoundingClientRect());
+    }
+  }
+
+  /**
+   * Sets the reference to the code textarea element and adjusts its height accordingly.
+   *
+   * @param content The ElementRef of the code textarea.
+   */
+  @ViewChild('codeTextarea') set content(content: ElementRef) {
+    if (content) {
+      this.codeTextarea = content;
+      const style = getComputedStyle(content.nativeElement);
+      this.maxHeight = getCssVariableValue(style, '--codeTextarea-max-height') || null;
+      this.minHeight = getCssVariableValue(style, '--codeTextarea-min-height') || 0;
+      this.adjustHeight();
+
+      content.nativeElement.focus({
+        preventScroll: true,
+      });
+      // console.log('codeTextarea:', this.codeTextarea?.nativeElement.getBoundingClientRect());
+    }
+  }
+
+  /**
+   * Adjusts the height of the code textarea to fit its content.
+   *
+   * @returns
+   */
+  public adjustHeight(): void {
+    const textarea = this.codeTextarea?.nativeElement as HTMLTextAreaElement;
+    if (!textarea) return;
+
+    textarea.style.height = 'auto';
+
+    const border = textarea.offsetHeight - textarea.clientHeight;
+
+    let target = textarea.scrollHeight + border;
+
+    if (this.maxHeight) target = Math.min(target, this.maxHeight);
+    target = Math.max(target, this.minHeight);
+
+    textarea.style.height = Math.ceil(target) + 'px';
+  }
+
+  /**
+   * Sets the view mode of the component, which determines whether the code is displayed in a read-only format or an editable textarea.
+   *
+   * @param mode
+   */
+  public setViewMode(mode: boolean) {
+    this.isViewMode = mode;
+    this.viewModeChange.emit();
   }
 }
