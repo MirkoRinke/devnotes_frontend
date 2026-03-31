eeb3f8b5b542cdb97c8dd534085b2f30922a217e
MirkoRinke <mirkorinke@htp.com>
2026-03-31 16:43:14 +0200

    feat(post-tech-stack): add control inputs for languages and technologies in tech stack selector

--- src/app/components/post/post-tech-stack-selector/post-tech-stack-selector.ts
+++ src/app/components/post/post-tech-stack-selector/post-tech-stack-selector.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import type { AvailableValuesInterface } from '../../../interfaces/available-values';
 
 @Component({
   selector: 'app-post-tech-stack-selector',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './post-tech-stack-selector.html',
   styleUrl: './post-tech-stack-selector.scss',
 })
-export class PostTechStackSelector {}
+export class PostTechStackSelector {
+  @Input() controlLanguages: FormControl | null = null;
+  @Input() controlTechnologies: FormControl | null = null;
+
+  availableValues: AvailableValuesInterface[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    console.log('controlLanguages', this.controlLanguages?.value);
+    console.log('controlTechnologies', this.controlTechnologies?.value);
+  }
+}
