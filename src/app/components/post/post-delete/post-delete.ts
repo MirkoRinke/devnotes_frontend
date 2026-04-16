e4e6e2642dde053de4706384bc66e2c3400bcfd6
MirkoRinke <mirkorinke@htp.com>
2026-04-16 15:44:45 +0200

    feat(post-delete): implement post deletion functionality with confirmation modal

--- src/app/components/post/post-delete/post-delete.ts
+++ src/app/components/post/post-delete/post-delete.ts
@@ -1,4 +1,12 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { SvgIconsService } from '../../../services/svg.icons.service';
+
+import { ApiService } from '../../../services/api.service';
+import { AuthService } from '../../../services/auth.service';
+
+import { ApiEndpointEnums } from '../../../enums/api-endpoint';
 
 @Component({
   selector: 'app-post-delete',
@@ -7,13 +15,50 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './post-delete.scss',
 })
 export class PostDelete {
-  @Input() mode: 'delete' = 'delete';
+  @Input() postId: number | null = null;
+  @Input() postUserId: number | null = null;
+  @Input() postTitle: string | null = null;
 
-  @Output() modeChange = new EventEmitter<'view'>();
+  @Output() closeModal = new EventEmitter<void>();
 
-  constructor() {}
+  constructor(
+    public svgIconsService: SvgIconsService,
+    private apiService: ApiService,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
 
-  switchMode(newMode: 'view') {
-    this.modeChange.emit(newMode);
+  /**
+   * Handle post deletion
+   *
+   * @param postId
+   * @param postUserId
+   * @returns
+   */
+  onDeletePost(postId: number, postUserId: number) {
+    if (this.authService.getCurrentUserId() !== postUserId) {
+      console.error('User is not the owner of the post');
+      return;
+    }
+
+    const url = `${ApiEndpointEnums.DELETE_POSTS}${postId}`;
+
+    this.apiService.delete(url).subscribe({
+      next: () => {
+        console.log('Post deleted successfully');
+        //TODO Navigate to the Post List or Home page after deletion
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.error('Error deleting post:', error);
+      },
+    });
+  }
+
+  /**
+   * Close the delete confirmation modal
+   */
+  public onClose() {
+    this.closeModal.emit();
   }
 }
