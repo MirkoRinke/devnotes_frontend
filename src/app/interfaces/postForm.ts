132138f9c5946e6216b140ab7271a3e5a66077f8
MirkoRinke <mirkorinke@htp.com>
2026-04-19 18:12:41 +0200

    feat(post-form): add resourceRefreshInterface for post update handling

--- src/app/interfaces/postForm.ts
+++ src/app/interfaces/postForm.ts
@@ -1,4 +1,12 @@
+import type { PostInterface } from './post';
+
 export interface TechStackSelectedValueInterface {
   name: string;
   entity: string;
+}
+
+export interface resourceRefreshInterface {
+  updatedPost: PostInterface;
+  entity: string | null;
+  entityValue: string | null;
 }
