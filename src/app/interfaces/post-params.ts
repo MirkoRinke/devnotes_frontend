8c1238721a20c5c490a38180038e167485da52fd
MirkoRinke <mirkorinke@htp.com>
2026-05-04 19:08:50 +0200

    refactor(post-params): update PostParamsInterface to inherit types from PostTypesParamsInterface

--- src/app/interfaces/post-params.ts
+++ src/app/interfaces/post-params.ts
@@ -1,7 +1,9 @@
+import type { PostTypesParamsInterface } from './post-types-params';
+
 export interface PostParamsInterface {
-  context: string | null;
-  endPoint: string | null;
-  selectedEntity: string | null;
-  selectedEntityValue: string | null;
-  selectedPostType: string | null;
+  context: PostTypesParamsInterface['context'];
+  endPoint: PostTypesParamsInterface['endPoint'];
+  selectedEntity: PostTypesParamsInterface['entity'];
+  selectedEntityValue: PostTypesParamsInterface['entityValue'];
+  selectedPostType: PostTypesParamsInterface['postType'];
 }
