f4355e152a92a70bb29c7e3926919f158de7513d
MirkoRinke <mirkorinke@htp.com>
2026-05-06 17:17:06 +0200

    refactor(post-types-params): update context type to use PageContextEnums for consistency

--- src/app/interfaces/post-types-params.ts
+++ src/app/interfaces/post-types-params.ts
@@ -1,7 +1,8 @@
 import { ApiEndpointEnums } from '../enums/api-endpoint';
+import { PageContextEnums } from '../enums/context';
 import { PostListAllowedEntitiesEnums } from '../enums/post-list-allowed-entities';
 export interface PostTypesParamsInterface {
-  context: string | null;
+  context: PageContextEnums | null;
   endPoint: keyof typeof ApiEndpointEnums | null;
   entity: PostListAllowedEntitiesEnums | null;
   entityValue: string | null;
