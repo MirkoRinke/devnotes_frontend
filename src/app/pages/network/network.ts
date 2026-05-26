a4f6e00bd06a4ea79a4a0644433aea578a36b795
MirkoRinke <mirkorinke@htp.com>
2026-05-26 18:16:04 +0200

    feat(network): implement action-placeholder for user engagement based on authentication status

--- src/app/pages/network/network.ts
+++ src/app/pages/network/network.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ActionPlaceholder } from '../../components/action-placeholder/action-placeholder';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-network',
-  imports: [],
+  imports: [ActionPlaceholder],
   templateUrl: './network.html',
   styleUrl: './network.scss',
 })
 export class Network {
-
+  constructor(public authService: AuthService) {}
 }
