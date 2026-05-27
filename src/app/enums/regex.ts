9f99521ad907cc5bfc14b18faf9fea97bf90300e
MirkoRinke <mirkorinke@htp.com>
2026-05-27 20:38:52 +0200

    feat(regex): add email and username regex patterns for validation

--- src/app/enums/regex.ts
+++ src/app/enums/regex.ts
@@ -1,4 +1,6 @@
 export enum RegexEnums {
   entityValue = '^[a-zA-Z0-9_,.\-:@#& /\\+\\*]+$',
   datepicker = '^\\d{4}-\\d{2}-\\d{2}$',
+  email = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$',
+  username = '^[a-zA-Z0-9._-]{2,}$',
 }
