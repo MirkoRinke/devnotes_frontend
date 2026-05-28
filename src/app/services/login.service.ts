7341921d70a2d027b00c545a3d5b7a6e6b867ace
MirkoRinke <mirkorinke@htp.com>
2026-05-28 20:15:59 +0200

    feat(login): enhance login method to save user ID and improve type safety

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,7 +7,8 @@ import { ApiService } from './api.service';
 
 import { ApiEndpointEnums } from '../enums/api-endpoint';
 
-import type { LoginFormInterface, LoginDataInterface } from '../interfaces/login-form';
+import type { LoginFormInterface, LoginDataInterface, LoginResponseInterface } from '../interfaces/login-form';
+import type { ApiResponseObjektInterface } from '../interfaces/api-response';
 
 @Injectable({
   providedIn: 'root',
@@ -18,7 +19,13 @@ export class LoginService {
     private apiService: ApiService,
   ) {}
 
-  login<T>(data: LoginFormInterface): Observable<T> {
+  /**
+   * Performs the login operation by sending the login data to the API and handling the response.
+   *
+   * @param data
+   * @returns
+   */
+  login(data: LoginFormInterface): Observable<ApiResponseObjektInterface<LoginResponseInterface>> {
     const deviceFingerprint = this.authService.getDeviceFingerprint();
     const deviceName = this.authService.getDeviceName();
 
@@ -28,10 +35,43 @@ export class LoginService {
       device_name: deviceName,
     };
 
-    return this.apiService.post<T>(ApiEndpointEnums.LOGIN, loginData).pipe(
-      tap((response: any) => {
-        console.log('Login erfolgreich:', response);
+    return this.apiService.post<ApiResponseObjektInterface<LoginResponseInterface>>(ApiEndpointEnums.LOGIN, loginData).pipe(
+      tap((response: ApiResponseObjektInterface<LoginResponseInterface>) => {
+        const token = response.data.data.accessToken || null;
+        const userId = response.data.data.user_id || null;
+        if (token && userId) {
+          this.saveToken(token);
+          this.saveUserId(userId);
+        }
       }),
     );
+  }
+
+  /**
+   * Saves the access token to local storage for later use in authenticated requests.
+   *
+   * @param token The access token to be saved.
+   */
+  saveToken(token: string) {
+    localStorage.setItem('accessToken', token);
+  }
+
+  /**
+   * Saves the user ID to local storage for later use in identifying the logged-in user.
+   *
+   * @param userId The user ID to be saved.
+   */
+  saveUserId(userId: number) {
+    localStorage.setItem('user_id', userId.toString());
+  }
+
+  /**
+   * Performs the logout operation by removing the access token and user ID from local storage, effectively logging the user out.
+   */
+  logout() {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('user_id');
+
+    //TODO: Implement logout logic, e.g., redirect to the login page.
   }
 }
