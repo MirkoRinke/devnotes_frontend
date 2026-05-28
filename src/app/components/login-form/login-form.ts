94349dfb4a54413488daffeeed4c543d9d024b4c
MirkoRinke <mirkorinke@htp.com>
2026-05-28 20:16:46 +0200

    feat(login-form): enhance form submission logic and integrate login service

--- src/app/components/login-form/login-form.ts
+++ src/app/components/login-form/login-form.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
+import { LoginService } from '../../services/login.service';
+
 import type { LoginFormErrorsInterface, LoginFormInterface } from '../../interfaces/login-form';
 
 import { emailOrUsernameValidator } from '../../utils/custom-validators';
@@ -15,7 +17,10 @@ import { RegexEnums } from '../../enums/regex';
 export class LoginForm {
   loginForm: FormGroup | null = null;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private loginService: LoginService,
+  ) {}
 
   ngOnInit() {
     this.createForm();
@@ -29,31 +34,43 @@ export class LoginForm {
   }
 
   onSubmit() {
-    if (this.loginForm?.valid) {
-      const formData = this.loginForm.value;
+    if (!this.loginForm) return;
 
-      const data: LoginFormInterface = {
-        password: formData.password,
-
-        // For Testing purposes, we set these to true by default.
-        privacy_policy_accepted: true,
-        terms_of_service_accepted: true,
-      };
-
-      const isEmail = new RegExp(RegexEnums.email).test(formData.identifier);
-      if (isEmail) {
-        data.email = formData.identifier;
-      } else {
-        data.user_name = formData.identifier;
-      }
-
-      console.log('Form submitted with data:', data);
-      //TODO Here Call LoginService to perform the login action, e.g.:
-    } else {
-      const errors = this.getFormErrors();
-      console.log('Form is invalid', errors);
-      this.loginForm?.markAllAsTouched();
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      this.getFormErrors();
+      return;
     }
+
+    const formData = this.loginForm.value;
+
+    const data: LoginFormInterface = {
+      password: formData.password,
+
+      // For Testing purposes, we set these to true by default.
+      privacy_policy_accepted: true,
+      terms_of_service_accepted: true,
+    };
+
+    const isEmail = new RegExp(RegexEnums.email).test(formData.identifier);
+    if (isEmail) {
+      data.email = formData.identifier;
+    } else {
+      data.user_name = formData.identifier;
+    }
+
+    this.login(data);
+  }
+
+  login(data: LoginFormInterface) {
+    this.loginService.login(data).subscribe({
+      next: (response) => {
+        // console.log('Login successful:', response);
+      },
+      error: (error) => {
+        // console.error('Login failed:', error);
+      },
+    });
   }
 
   getFormErrors() {
