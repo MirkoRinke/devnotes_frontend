5cf0b1fcb3f03905cde94c0f79d877e6d076fd6a
MirkoRinke <mirkorinke@htp.com>
2026-06-04 17:58:43 +0200

    feat(goodbye): add goodbye component with template, styles, and tests

---
+++ src/app/pages/goodbye/goodbye.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goodbye',
+  imports: [],
+  templateUrl: './goodbye.html',
+  styleUrl: './goodbye.scss',
+})
+export class Goodbye {
+
+}
