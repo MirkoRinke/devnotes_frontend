10491f6f43e92af60d6c85440303e0d9fc050a4c
MirkoRinke <mirkorinke@htp.com>
2026-06-04 18:01:38 +0200

    feat(header): implement authentication logic for login/logout in header component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -3,10 +3,18 @@ import { RouterModule } from '@angular/router';
 
 import { PageNavigation } from '../page-navigation/page-navigation';
 
+import { AuthService } from '../../services/auth.service';
+import { LogoutService } from '../../services/logout.service';
+
 @Component({
   selector: 'app-header',
   imports: [RouterModule, PageNavigation],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  constructor(
+    public authService: AuthService,
+    public logoutService: LogoutService,
+  ) {}
+}
