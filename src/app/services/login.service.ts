3c6ac56e46292c4c086746c24667845f0855185c
MirkoRinke <mirkorinke@htp.com>
2026-06-09 16:49:58 +0200

    feat(login-service): refactor login data handling to use AuthStorageService for saving token and user ID

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -6,6 +6,7 @@ import { AuthService } from './auth.service';
 import { ApiService } from './api.service';
 
 import { ApiEndpointEnums } from '../enums/api-endpoint';
+import { AuthStorageService } from './auth-storage.service';
 
 import type { LoginFormInterface, LoginDataInterface, LoginResponseInterface } from '../interfaces/login-form';
 import type { ApiResponseObjektInterface } from '../interfaces/api-response';
@@ -17,6 +18,7 @@ export class LoginService {
   constructor(
     private authService: AuthService,
     private apiService: ApiService,
+    private authStorageService: AuthStorageService,
   ) {}
 
   /**
@@ -40,28 +42,9 @@ export class LoginService {
         const token = response.data.data.accessToken || null;
         const userId = response.data.data.user_id || null;
         if (token && userId) {
-          this.saveToken(token);
-          this.saveUserId(userId);
+          this.authStorageService.saveLoginData(token, userId);
         }
       }),
     );
-  }
-
-  /**
-   * Saves the access token to local storage for later use in authenticated requests.
-   *
-   * @param token The access token to be saved.
-   */
-  saveToken(token: string) {
-    localStorage.setItem('accessToken', token);
-  }
-
-  /**
-   * Saves the user ID to local storage for later use in identifying the logged-in user.
-   *
-   * @param userId The user ID to be saved.
-   */
-  saveUserId(userId: number) {
-    localStorage.setItem('user_id', userId.toString());
   }
 }
