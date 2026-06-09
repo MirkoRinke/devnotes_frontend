6ac407f94b85a2197dde8295dcf2754a8b98e358
MirkoRinke <mirkorinke@htp.com>
2026-06-09 17:00:55 +0200

    feat(api-error-handling): refactor error handling methods for improved clarity and structure

--- src/app/services/api-error-handling.service.ts
+++ src/app/services/api-error-handling.service.ts
@@ -1,46 +1,92 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import type { BackendErrorResponseInterface, BusinessActionInterface } from '../interfaces/error-handling';
+import { AuthStorageService } from './auth-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiErrorHandlingService {
-  processTechnicalError(error: BackendErrorResponseInterface): void {}
+  constructor(
+    private router: Router,
+    private authStorageService: AuthStorageService,
+  ) {}
 
-  processValidationIssues(error: BackendErrorResponseInterface): void {}
-
-  processBusinessEvent(error: BackendErrorResponseInterface): BusinessActionInterface {
+  handleApiError(error: BackendErrorResponseInterface): BusinessActionInterface | void {
     switch (error.code) {
-      case 403:
-        if (error.errors === 'PRIVACY_POLICY_NOT_ACCEPTED' || error.errors === 'TERMS_OF_SERVICE_NOT_ACCEPTED') {
-          return {
-            mustAcceptConditions: true,
-            messages: {
-              message: 'Es gab neue Nutzungsbedingungen oder Datenschutzrichtlinien',
-              messageType: 'info',
-            },
-          };
-        }
-        break;
-
       case 401:
-        if (error.errors === 'CREDENTIALS_INCORRECT') {
-          return {
-            messages: {
-              message: 'E-Mail-Adresse / Benutzername oder Passwort ist falsch.',
-              messageType: 'error',
-            },
-          };
-        }
-        break;
+        return this.handle401(error);
+      case 403:
+        return this.handle403(error);
+      default:
+        return this.handleDefault(error);
     }
+  }
 
+  /**
+   * Default error handling for unrecognized error codes.
+   * Returns a generic error message to be displayed to the user.
+   *
+   * @param error
+   * @returns
+   */
+  private handleDefault(error: BackendErrorResponseInterface): BusinessActionInterface | void {
+    console.warn('Unbekannter Fehlercode:', error.code);
+    console.log('Fehlerdetails:', error);
     return {
       messages: {
         message: 'Es ist ein Fehler aufgetreten. Bitte erneut versuchen.',
         messageType: 'error',
       },
     };
+  }
+
+  /**
+   * Handles 401 Unauthorized errors.
+   *
+   * @param error
+   * @returns
+   */
+  private handle401(error: BackendErrorResponseInterface): BusinessActionInterface | void {
+    if (error.errors === 'CREDENTIALS_INCORRECT') {
+      return {
+        messages: {
+          message: 'E-Mail-Adresse / Benutzername oder Passwort ist falsch.',
+          messageType: 'error',
+        },
+      };
+    }
+    if (error.errors === 'UNAUTHORIZED') {
+      this.authStorageService.clearLoginData();
+      this.router.navigate(['/login']);
+      return;
+    }
+    return this.handleDefault(error);
+  }
+
+  /**
+   * Handles 403 Forbidden errors.
+   *
+   * @param error
+   * @returns
+   */
+  private handle403(error: BackendErrorResponseInterface): BusinessActionInterface | void {
+    if (error.errors === 'PRIVACY_POLICY_NOT_ACCEPTED' || error.errors === 'TERMS_OF_SERVICE_NOT_ACCEPTED') {
+      const isLoginPage = this.router.url.includes('/login');
+      if (!isLoginPage) {
+        //TODO replace later with a specific page. /compliance-check or something like that
+        this.router.navigate(['/login']);
+        return;
+      }
+      return {
+        mustAcceptConditions: true,
+        messages: {
+          message: 'Es gab neue Nutzungsbedingungen oder Datenschutzrichtlinien',
+          messageType: 'info',
+        },
+      };
+    }
+    return this.handleDefault(error);
   }
 }
