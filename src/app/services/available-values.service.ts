77898f18e7787b60c70bb0654e5b7d7da71e9726
MirkoRinke <mirkorinke@htp.com>
2026-06-09 16:49:19 +0200

    feat(available-values-service): remove console logging for successful data retrieval and error handling

--- src/app/services/available-values.service.ts
+++ src/app/services/available-values.service.ts
@@ -26,11 +26,7 @@ export class AvailableValuesService {
     const requests = params.map((param) => this.apiService.get<ApiResponseArrayInterface<AvailableValuesInterface>>(`${ApiEndpointEnums[endPoint as keyof typeof ApiEndpointEnums]}${param}`));
     return forkJoin(requests).pipe(
       map((responses) => responses.flatMap((response) => response.data.data)),
-      tap((data) => {
-        // console.log('Successfully retrieved values:', data);
-      }),
       catchError((error) => {
-        console.error('Error fetching available values');
         return of([]);
       }),
     );
