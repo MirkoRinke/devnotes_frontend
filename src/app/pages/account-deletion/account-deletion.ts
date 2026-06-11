adb9379c20f53155c630d3c57308b931a5df0482
MirkoRinke <mirkorinke@htp.com>
2026-06-11 19:35:10 +0200

    feat(account-deletion): replace placeholder text with DeleteAccount component

--- src/app/pages/account-deletion/account-deletion.ts
+++ src/app/pages/account-deletion/account-deletion.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { DeleteAccount } from '../../components/delete-account/delete-account';
 
 @Component({
   selector: 'app-account-deletion',
-  imports: [],
+  imports: [DeleteAccount],
   templateUrl: './account-deletion.html',
   styleUrl: './account-deletion.scss',
 })
-export class AccountDeletion {
-
-}
+export class AccountDeletion {}
