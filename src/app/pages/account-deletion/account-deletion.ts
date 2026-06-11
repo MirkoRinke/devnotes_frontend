c16dcd9b68d58c265b106812fca2cb85ea63043d
MirkoRinke <mirkorinke@htp.com>
2026-06-11 16:39:52 +0200

    feat(account-deletion): add account deletion component with template, styles, and tests

---
+++ src/app/pages/account-deletion/account-deletion.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-deletion',
+  imports: [],
+  templateUrl: './account-deletion.html',
+  styleUrl: './account-deletion.scss',
+})
+export class AccountDeletion {
+
+}
