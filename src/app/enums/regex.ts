d4bf11850daa240cf6ef493e1b50c057ead17fea
MirkoRinke <mirkorinke@htp.com>
2026-06-12 21:34:43 +0200

    feat(regex): add digitsOnly regex pattern to RegexEnums

--- src/app/enums/regex.ts
+++ src/app/enums/regex.ts
@@ -3,4 +3,5 @@ export enum RegexEnums {
   datepicker = '^\\d{4}-\\d{2}-\\d{2}$',
   email = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$',
   username = '^[a-zA-Z0-9._-]{2,}$',
+  digitsOnly = '\\d+',
 }
