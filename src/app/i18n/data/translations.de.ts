7ed0c2cb8d540aec06d5ce611b979fc430c6ccf6
MirkoRinke <mirkorinke@htp.com>
2026-06-18 20:05:16 +0200

    feat(translation): add German translations for authentication error, info, and success messages

--- src/app/i18n/data/translations.de.ts
+++ src/app/i18n/data/translations.de.ts
@@ -31,4 +31,41 @@ export const CONTENT_DE: Content = {
       description: 'Anleitungen & Tutorials. Finde oder erstelle leicht verständliche Anleitungen für neue Skills.',
     },
   },
+  Auth: {
+    error: {
+      identifier: {
+        required: 'E-Mail-Adresse / Benutzernamen eingeben.',
+        login_identifier_invalid: 'Die E-Mail-Adresse oder der Benutzername ist ungültig.',
+        maxlength: 'Die E-Mail-Adresse oder der Benutzername ist ungültig.',
+      },
+      password: {
+        required: 'Passwort eingeben.',
+        minlength: 'Das Passwort muss mindestens 8 Zeichen lang sein.',
+        maxlength: 'Das Passwort darf maximal 255 Zeichen lang sein.',
+      },
+      acceptedConditions: {
+        required: 'Bitte Nutzungsbedingungen & Datenschutzrichtlinie akzeptieren.',
+      },
+      login: {
+        UNKNOWN_ERROR: 'Es ist ein Fehler aufgetreten. Bitte erneut versuchen.',
+        CREDENTIALS_INCORRECT: 'E-Mail-Adresse / Benutzername oder Passwort ist falsch.',
+        ACCOUNT_DELETION_FORBIDDEN: 'Das Löschen des Kontos ist nicht möglich.',
+        ACCOUNT_SUSPENDED: 'Ihr Konto wurde für {days} Tage gesperrt.',
+      },
+      deleteAccount: {},
+    },
+    info: {
+      login: {
+        MUST_ACCEPT_CONDITIONS: 'Es gab neue Nutzungsbedingungen oder Datenschutzrichtlinien',
+      },
+    },
+    success: {
+      login: {
+        SUCCESSFUL: 'Login erfolgreich. Weiterleitung...',
+      },
+      acceptedConditions: {
+        ACCEPTED_CONDITIONS: 'Nutzungsbedingungen & Datenschutzrichtlinie akzeptiert.',
+      },
+    },
+  },
 };
