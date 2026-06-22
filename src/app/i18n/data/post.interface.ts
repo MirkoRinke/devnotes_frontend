63a956edd0efac59b7a8f5561ce99e491b6c5638
MirkoRinke <mirkorinke@htp.com>
2026-06-22 21:57:53 +0200

    feat(post-messages): enhance error and info message interfaces for improved error handling

--- src/app/i18n/data/post.interface.ts
+++ src/app/i18n/data/post.interface.ts
@@ -7,28 +7,42 @@ export interface PostMessagesInterface {
 // #region Error Messages
 interface ErrorMessages {
   delete?: CustomErrorMessages;
+
+  post_type?: CustomErrorMessages;
+  category?: CustomErrorMessages;
+  syntax_highlighting?: CustomErrorMessages;
+  status?: CustomErrorMessages;
+  title?: CustomErrorMessages;
+  description?: CustomErrorMessages;
+  code?: CustomErrorMessages;
+  language_or_tech_required?: CustomErrorMessages;
 }
 
 interface CustomErrorMessages {
   UNKNOWN_ERROR: string;
   NO_PERMISSION?: string;
   CONFIRMATION_TEXT_MISMATCH?: string;
+  E?: string;
+  syntax_highlighting_required?: string;
 }
 // #endregion
 
 // #region Info Messages
 interface InfoMessages {
   delete?: CustomInfoInterface;
+  syntax_highlighting?: CustomInfoInterface;
 }
 
 interface CustomInfoInterface {
   CONFIRMATION_TEXT_REQUIRED?: string;
+  syntax_highlighting_required?: string;
 }
 // #endregion
 
 // #region Success Messages
 interface SuccessMessages {
   delete?: CustomSuccessInterface;
+  errorCode?: CustomSuccessInterface;
 }
 
 interface CustomSuccessInterface {
