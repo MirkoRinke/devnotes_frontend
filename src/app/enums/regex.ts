e85d5b85588ace978db5d2073209ca8c662548d3
MirkoRinke <mirkorinke@htp.com>
2026-07-07 23:55:43 +0200

    feat(regex): update username regex to allow spaces

--- src/app/enums/regex.ts
+++ src/app/enums/regex.ts
@@ -2,7 +2,7 @@ export enum RegexEnums {
   entityValue = '^[a-zA-Z0-9_,.\-:@#& /\\+\\*]+$',
   datepicker = '^\\d{4}-\\d{2}-\\d{2}$',
   email = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$',
-  username = '^[a-zA-Z0-9._-]{2,}$',
+  username = '^[a-zA-Z0-9._\\- ]{2,}$',
   digitsOnly = '\\d+',
   hasUpperCase = '[A-Z]',
   hasLowerCase = '[a-z]',
