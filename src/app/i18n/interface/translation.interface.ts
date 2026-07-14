6ade68a17918b7358e820399313c5cd02826a6bc
MirkoRinke <mirkorinke@htp.com>
2026-07-14 22:32:54 +0200

    feat(translation): extend Content interface to include LegalContent for improved localization support

--- src/app/i18n/interface/translation.interface.ts
+++ src/app/i18n/interface/translation.interface.ts
@@ -2,11 +2,20 @@ import { GlobalMessagesInterface } from './global.interface';
 import { AuthMessagesInterface } from './auth.interface';
 import { PostTypesMessagesInterface } from './post-types.interface';
 import { PostMessagesInterface } from './post.interface';
+import { ImprintMessagesInterface } from './imprint.interface';
+import { PrivacyMessagesInterface } from './privacy.interface';
+import { TermsMessagesInterface } from './terms.interface';
 
-export interface Content extends ContentUI, Notifications {}
+export interface Content extends ContentUI, LegalContent, Notifications {}
 
 export interface ContentUI {
   PostTypes: PostTypesMessagesInterface;
+}
+
+export interface LegalContent {
+  Imprint: ImprintMessagesInterface;
+  Privacy: PrivacyMessagesInterface;
+  Terms: TermsMessagesInterface;
 }
 
 export interface Notifications {
