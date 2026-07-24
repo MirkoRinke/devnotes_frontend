b8ce8ff8ffc1e577e1fac43e30b70b499f68a450
MirkoRinke <mirkorinke@htp.com>
2026-07-25 00:39:00 +0200

    feat(header): replace placeholder with user control area component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,19 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { PageNavigation } from '../page-navigation/page-navigation';
-
-import { AuthService } from '../../services/auth.service';
-import { LogoutService } from '../../services/logout.service';
+import { UserControlArea } from '../user-control-area/user-control-area';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, PageNavigation],
+  imports: [RouterModule, PageNavigation, UserControlArea],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-  constructor(
-    public authService: AuthService,
-    public logoutService: LogoutService,
-  ) {}
-}
+export class Header {}
