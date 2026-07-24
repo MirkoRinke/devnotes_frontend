bd21959c47a05408c89b199e5caa6f6ab2a831f2
MirkoRinke <mirkorinke@htp.com>
2026-07-25 00:12:56 +0200

    feat(user-auth): implement user authentication component with template, styles, and tests

---
+++ src/app/components/user-auth/user-auth.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-user-auth',
+  imports: [RouterModule],
+  templateUrl: './user-auth.html',
+  styleUrl: './user-auth.scss',
+})
+export class UserAuth {}
