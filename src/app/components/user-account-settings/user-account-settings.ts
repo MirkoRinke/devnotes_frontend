f85fbabfc918b8670da442b543753ae5be65162b
MirkoRinke <mirkorinke@htp.com>
2026-07-25 20:07:40 +0200

    feat(user-account-settings): add user account settings component with template, styles, and tests

---
+++ src/app/components/user-account-settings/user-account-settings.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-account-settings',
+  imports: [],
+  templateUrl: './user-account-settings.html',
+  styleUrl: './user-account-settings.scss',
+})
+export class UserAccountSettings {
+
+}
