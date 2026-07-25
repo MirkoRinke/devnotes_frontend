b9a5439ac30bfacb4e3d05744845ea350740a9de
MirkoRinke <mirkorinke@htp.com>
2026-07-25 19:00:14 +0200

    feat(auth-storage): add BehaviorSubject for authentication state management

--- src/app/services/auth-storage.service.ts
+++ src/app/services/auth-storage.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthStorageService {
+  private logged$ = new BehaviorSubject<void>(undefined);
+
+  readonly authChanged$ = this.logged$.asObservable();
+
   /**
    * Saves both the access token and user ID to local storage, typically called after a successful login.
+   * and emits a notification to subscribers that the authentication state has changed.
    *
    * @param token The access token to be saved.
    * @param userId The user ID to be saved.
@@ -13,13 +19,16 @@ export class AuthStorageService {
   saveLoginData(token: string, userId: number) {
     localStorage.setItem('accessToken', token);
     localStorage.setItem('user_id', userId.toString());
+    this.logged$.next();
   }
 
   /**
    * Clears the login data (access token and user ID) from local storage, effectively logging the user out.
+   * and emits a notification to subscribers that the authentication state has changed.
    */
   clearLoginData() {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('user_id');
+    this.logged$.next();
   }
 }
