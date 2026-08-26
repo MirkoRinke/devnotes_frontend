9c27c091f61a3860986913abb209f783c85fe2ab
MirkoRinke <mirkorinke@htp.com>
2026-08-27 01:42:53 +0200

    feat(no-results): add NoResults component with template, styles, and tests

---
+++ src/app/components/no-results/no-results.ts
@@ -0,0 +1,37 @@
+import { Component, Input } from '@angular/core';
+
+import { TranslatePipe } from '../../i18n/translate-pipe';
+
+import type { ParamsInterface } from '../../interfaces/error-handling';
+
+@Component({
+  selector: 'app-no-results',
+  imports: [TranslatePipe],
+  templateUrl: './no-results.html',
+  styleUrl: './no-results.scss',
+})
+export class NoResults {
+  @Input() params: ParamsInterface | null = null;
+
+  /**
+   * Returns the aria-label key based on the provided params.
+   *
+   * @returns
+   */
+  ariaLabelKey(): string {
+    if (this.params) {
+      return 'noResultsParams';
+    } else {
+      return 'noResults';
+    }
+  }
+
+  /**
+   * Returns the aria-label parameters based on the provided params.
+   *
+   * @returns
+   */
+  ariaLabelParams(): ParamsInterface | null {
+    return this.params ? this.params : null;
+  }
+}
