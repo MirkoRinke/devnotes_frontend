d0cae38defbf1a615e09f14362ddac26144f9b0c
MirkoRinke <mirkorinke@htp.com>
2026-08-27 02:00:17 +0200

    feat(translate-pipe): update transform method to accept optional parameters for translations

--- src/app/i18n/translate-pipe.ts
+++ src/app/i18n/translate-pipe.ts
@@ -1,6 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { TranslationService } from './translation.service';
 
+import type { ParamsInterface } from '../interfaces/error-handling';
+
 @Pipe({
   name: 'translate',
   pure: false,
@@ -12,9 +14,10 @@ export class TranslatePipe implements PipeTransform {
    * Transforms a translation key into its corresponding translated string using the TranslationService.
    *
    * @param key The translation key to be transformed (e.g., "PostTypes.feedback.title").
+   * @param params Optional parameters to be used in the translation (e.g., { heading: 'Feedback' }).
    * @returns The translated string if found, or "quak" if the translation is not found or if the path is invalid.
    */
-  transform(key: string): string {
-    return this.translationService.getTranslation(key);
+  transform(key: string, params?: ParamsInterface | null): string {
+    return this.translationService.getTranslation(key, params);
   }
 }
