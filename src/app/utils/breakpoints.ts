07dacabdb9ebc856742743b3e490a84304eed01b
MirkoRinke <mirkorinke@htp.com>
2026-09-17 22:23:02 +0200

    feat(breakpoints): add responsive design breakpoints for consistent layout

---
+++ src/app/utils/breakpoints.ts
@@ -0,0 +1,13 @@
+/**
+ * Breakpoints for responsive design. The same as defined in _var.scss
+ */
+export const BREAKPOINTS = {
+  ultra: 3440,
+  wqhd: 2560,
+  fullHd: 1920,
+  hd: 1280,
+  md: 768,
+  sm: 640,
+  xs: 450,
+  xxs: 384,
+} as const;
